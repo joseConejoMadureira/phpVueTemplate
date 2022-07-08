977e27eeb747fafe1e05e9a700f24c867437ce73
joseEstudos <jose.games15@outlook.com>
2022-07-08 15:33:36 -0300

    config

--- backend/config/database copy.php
+++ backend/config/database copy.php
@@ -3,3 +3,4 @@ define("DATABASE_HOST", "localhost");
 define("DATABASE_USER", "root");
 define("DATABASE_PASS", "");
 define("DATABASE_NAME", "test-vista-software");
+?>
--- backend/config/database.php
+++ backend/config/database.php
@@ -1,5 +1,8 @@
 <?php
+
 define("DATABASE_HOST", "localhost");
 define("DATABASE_USER", "root");
 define("DATABASE_PASS", "");
 define("DATABASE_NAME", "test-vista-software");
+
+?>
