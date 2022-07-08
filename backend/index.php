9aa03053de05922c87af4d125bb0e5247cd69c9e
joseEstudos <jose.games15@outlook.com>
2022-07-08 15:52:12 -0300

    estrutura

---
+++ backend/index.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+//request
+
+
+switch($uri)
+{
+    case 'x' :
+        
+
+    break;
+
+    case 'y' :
+        
+
+    break;
+
+    case 'z' :
+        
+
+    break;
+
+   
+}
