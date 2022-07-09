cb34610e30e39ab6d276411310a9bcade54ccce1
joseEstudos <jose.games15@outlook.com>
2022-07-09 19:28:45 -0300

    logs

--- backend/index.php
+++ backend/index.php
@@ -6,10 +6,12 @@ require_once __DIR__ . "/vendor/autoload.php";
 LogsW::write(json_encode($_SERVER));
 
 //request
-$uri = 'w';
+$uri = 'x';
 
 switch ($uri) {
     case 'x':
+        LogsW::write('CASE X');
+
         break;
     case 'y':
         break;
