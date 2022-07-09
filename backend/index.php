9ee4f24591733919bf76595eaea26dacd403b2d3
joseEstudos <jose.games15@outlook.com>
2022-07-09 20:39:09 -0300

    logs

--- backend/index.php
+++ backend/index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . "/vendor/autoload.php";
   
 LogsW::write(json_encode($_SERVER));
 LogsW::write(json_encode($_SERVER['REQUEST_METHOD']));
-LogsW::write(json_encode($_SERVER['REQUEST_URI']));
+LogsW::write($_SERVER['REQUEST_URI']);
 LogsW::write(json_encode($_REQUEST));
 
 
