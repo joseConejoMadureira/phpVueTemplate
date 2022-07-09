d65814d8d527d250824b7ae16a83d88a17b684b1
joseEstudos <jose.games15@outlook.com>
2022-07-09 18:50:38 -0300

    identacao

--- backend/index.php
+++ backend/index.php
@@ -1,24 +1,17 @@
 <?php
 require_once __DIR__ . "/vendor/autoload.php";
 
- var_dump($_REQUEST);
+var_dump($_REQUEST);
 
 var_dump($_SERVER);
 //request
 $uri = 'w';
+
 switch ($uri) {
     case 'x':
-
-
         break;
-
     case 'y':
-
-
         break;
-
     case 'z':
-
-
         break;
 }
