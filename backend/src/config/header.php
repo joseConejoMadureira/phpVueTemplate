45cc42945a16137063e5b2fbe32885d3fcec0383
joseEstudos <jose.games15@outlook.com>
2022-07-09 21:51:30 -0300

    header

--- backend/src/config/header.php
+++ backend/src/config/header.php
@@ -3,6 +3,9 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Key, Authorization');
 header('Access-Control-Allow-Credentials: true');
+header('Content-Type: application/json');
+header('Content-Type: text/plain');
+header('Content-Type: */* ');
 
 foreach (getallheaders() as $name => $value) {
   LogsW::write("$name: $value");
