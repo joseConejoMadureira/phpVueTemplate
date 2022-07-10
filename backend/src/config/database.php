5f753c32ef66e0f814e71be688c441122f0733b6
joseEstudos <jose.games15@outlook.com>
2022-07-10 20:53:40 -0300

    databse

--- backend/src/config/database.php
+++ backend/src/config/database.php
@@ -4,5 +4,5 @@ namespace src\config;
 define("DATABASE_HOST", "localhost");
 define("DATABASE_USER", "root");
 define("DATABASE_PASS", "");
-define("DATABASE_NAME", "");
+define("DATABASE_NAME", "teste");
 
