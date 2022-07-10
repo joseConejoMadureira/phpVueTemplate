3da1a80d1c7b57c9611b36ab7b403569747304a0
joseEstudos <jose.games15@outlook.com>
2022-07-10 12:31:16 -0300

    index

--- backend/index.php
+++ backend/index.php
@@ -1,21 +1,29 @@
 <?php
 require_once __DIR__ . "/vendor/autoload.php";
 //request
-$uri = 'x';
+$uri = explode("/", $_SERVER['REQUEST_URI']);
+$method = $_SERVER['REQUEST_METHOD'];
+$model = $uri[3];
 
-switch ($uri) {
-    case 'x':
-        LogsW::write('CASE X');
+switch ($model) {
+    case 'product':
+        LogsW::write('CASE: product');
 
         break;
     case 'y':
         break;
     case 'z':
         break;
+    default:
+        LogsW::write('CASE: 404');
+        break;
 }
 
 //log
 $data = json_decode(file_get_contents("php://input"));
-$price = $data->price;
 LogsW::write(json_encode($data));
 LogsW::write($price);
+foreach ($uri as $value) {
+    LogsW::write($value);
+}
+LogsW::write($uri[3]);
