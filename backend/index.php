469cec40655dff2e820e1f3adda6d471ebaec8e4
joseEstudos <jose.games15@outlook.com>
2022-07-10 20:46:52 -0300

    comentarios

--- backend/index.php
+++ backend/index.php
@@ -10,6 +10,7 @@ $data = json_decode(file_get_contents("php://input"));
 $data = json_encode($data);
 
 switch ($service) {
+        //PRODUCT
     case 'product':
         LogsW::write('CASE: product');
         $product  = new  ProductService($uri, $method, $data);
@@ -40,10 +41,13 @@ switch ($service) {
                 die();
         }
         break;
+        // Y
     case 'y':
         break;
+        // Z
     case 'z':
         break;
+        // 404 
     default:
         LogsW::write('CASE: 404');
         header("HTTP/1.1 404 Not Found");
