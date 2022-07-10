270e65e337747c8a0e2b727eb3859ef8936670d1
joseEstudos <jose.games15@outlook.com>
2022-07-10 20:54:01 -0300

    database

--- backend/src/config/database.php
+++ backend/src/config/database.php
@@ -2,7 +2,7 @@
 namespace src\config;
 
 define("DATABASE_HOST", "localhost");
-define("DATABASE_USER", "root");
+define("DATABASE_USER", "postgres");
 define("DATABASE_PASS", "");
 define("DATABASE_NAME", "teste");
 
