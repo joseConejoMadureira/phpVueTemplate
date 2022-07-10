a1c9aa0b47e0da2af500bbe5484c9471e9c55fc5
joseEstudos <jose.games15@outlook.com>
2022-07-09 21:59:11 -0300

    header

--- backend/src/config/header.php
+++ backend/src/config/header.php
@@ -4,6 +4,7 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 86400");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header('Access-Control-Allow-Credentials: true');
 
 
 foreach (getallheaders() as $name => $value) {
