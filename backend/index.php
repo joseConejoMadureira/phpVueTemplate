c108a7953b86df236727d111117f717d0e5db7af
joseEstudos <jose.games15@outlook.com>
2022-07-09 21:42:38 -0300

    header logs

--- backend/index.php
+++ backend/index.php
@@ -1,11 +1,6 @@
 <?php
 require_once __DIR__ . "/vendor/autoload.php";
-  
-  
-LogsW::write(json_encode($_SERVER));
-LogsW::write(json_encode($_SERVER['REQUEST_METHOD']));
-LogsW::write($_SERVER['REQUEST_URI']);
-LogsW::write(json_encode($_REQUEST));
+
 
 
 //request
--- backend/src/config/header.php
+++ backend/src/config/header.php
@@ -1,6 +1,13 @@
 <?php
-  header('Access-Control-Allow-Origin: *');
-  header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
-  header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Key, Authorization');
-  header('Access-Control-Allow-Credentials: true');
-  
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Key, Authorization');
+header('Access-Control-Allow-Credentials: true');
+
+foreach (getallheaders() as $name => $value) {
+  LogsW::write("$name: $value");
+}
+LogsW::write(json_encode($_SERVER));
+LogsW::write(json_encode($_SERVER['REQUEST_METHOD']));
+LogsW::write($_SERVER['REQUEST_URI']);
+LogsW::write(json_encode($_REQUEST));
