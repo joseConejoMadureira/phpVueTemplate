42a9cf1c61c7a31d03ca56f9b16f31276ee0e2d1
joseEstudos <jose.games15@outlook.com>
2022-07-09 21:02:40 -0300

    header

--- backend/src/config/header.php
+++ backend/src/config/header.php
@@ -3,4 +3,4 @@
   header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
   header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Key, Authorization');
   header('Access-Control-Allow-Credentials: true');
-  header("Access-Control-Allow-Headers: X-Requested-With");
+  
