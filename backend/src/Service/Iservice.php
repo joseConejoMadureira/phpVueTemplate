df1bcebb6b346cf1fc049d31159afe2ed69e32a6
joseEstudos <jose.games15@outlook.com>
2022-07-11 14:27:55 -0300

    iservice

--- backend/src/Service/Iservice.php
+++ backend/src/Service/Iservice.php
@@ -1,11 +1,13 @@
-<?php 
- interface Iservice 
+<?php
+interface Iservice
 {
+    public $uri;
+    public $method;
+    public $data;
     public function action();
     public function read();
     public function readById();
     public function update();
     public function delete();
     public function route();
-    
 }
