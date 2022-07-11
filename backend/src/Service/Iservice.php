9abb5fcf8c4521326e3e8ee1ca8352d6741a8b19
joseEstudos <jose.games15@outlook.com>
2022-07-11 15:09:07 -0300

    service construtor

--- backend/src/Service/Iservice.php
+++ backend/src/Service/Iservice.php
@@ -1,9 +1,12 @@
 <?php
+namespace Service;
 interface Iservice
 {
     public $uri;
     public $method;
     public $data;
+    public function __construct($uri, $method, $data);
+
     public function action();
     public function read();
     public function readById();
