b501351c9995f4ac98917d597487013e562dfb51
joseEstudos <jose.games15@outlook.com>
2022-07-12 20:12:46 -0300

    daoProduct

--- backend/src/Dao/DaoProduct.php
+++ backend/src/Dao/DaoProduct.php
@@ -2,16 +2,9 @@
 namespace src\Dao;
 class DaoProduct
 {
-    public static $instance;
-    public static function getInstance()
-    {
-        if (!isset(self::$instance))
-            self::$instance = new DaoProduct();
-
-        return self::$instance;
-    }
+   
     public function read(){
-        $sql = "select * from teste";
+        $sql = "select * from products";
         $p_sql = ConnectionPDO::getInstance()->prepare($sql);
         $p_sql->execute();
     }
