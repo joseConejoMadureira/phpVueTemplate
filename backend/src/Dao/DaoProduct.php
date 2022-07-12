bc6903e1f4282c0eebee3eb4726a7bf939bc5bb6
joseEstudos <jose.games15@outlook.com>
2022-07-12 20:52:42 -0300

    PDO::FETCH_ASSOC

--- backend/src/Dao/DaoProduct.php
+++ backend/src/Dao/DaoProduct.php
@@ -1,12 +1,13 @@
 <?php
 namespace src\Dao;
 use LogsW;
+use PDO;
 class DaoProduct
 {
    
     public function read(){
         $sql = "select * from products";
         $p_sql = ConnectionPDO::getInstance()->query($sql);  
-        return json_encode($p_sql->fetchAll());
+        return json_encode($p_sql->fetchAll(PDO::FETCH_ASSOC));
     }
 }
