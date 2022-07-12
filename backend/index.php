6ebabcfa1e1b465aa97beade221cbdce14fa6efc
joseEstudos <jose.games15@outlook.com>
2022-07-12 19:24:00 -0300

    espaços

--- backend/index.php
+++ backend/index.php
@@ -9,7 +9,6 @@ $method = $_SERVER['REQUEST_METHOD'];
 $service = $uri[3];
 $data = json_decode(file_get_contents("php://input"));
 $data = json_encode($data);
-
 switch ($service) {
         //PRODUCT
     case 'product':
