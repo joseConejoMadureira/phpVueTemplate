4d71f86a9ba9b239005e3c27d5c78658c04f6618
joseEstudos <jose.games15@outlook.com>
2022-07-12 21:08:27 -0300

    exception

--- backend/src/Dao/DaoProduct.php
+++ backend/src/Dao/DaoProduct.php
@@ -2,12 +2,21 @@
 namespace src\Dao;
 use LogsW;
 use PDO;
+use PDOException;
 class DaoProduct
 {
    
     public function read(){
         $sql = "select * from products";
-        $p_sql = ConnectionPDO::getInstance()->query($sql);  
-        return json_encode($p_sql->fetchAll(PDO::FETCH_ASSOC));
+        try{
+            $p_sql = ConnectionPDO::getInstance()->query($sql);  
+            $result = $p_sql->fetchAll(PDO::FETCH_ASSOC);
+          
+        }catch(PDOException $e){
+            LogsW::write($e);
+            return json_encode('error');
+        }
+        
+        return json_encode($result);
     }
 }
